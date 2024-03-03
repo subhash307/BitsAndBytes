8e7e06147a2d851f72613ed3c149d2236f20c451
SubhashKumar <subhash.kumar307@gmail.com>
2024-03-04 02:17:34 +0530

    Stream API important questions

---
+++ src/Employee.java
@@ -0,0 +1,59 @@
+package src;
+
+import java.util.Date;
+
+public class Employee {
+    private String name;
+    private Double salary;
+    private Date joiningDate;
+    private String gender;
+
+    public Employee(String name, Double salary, Date joiningDate, String gender) {
+        this.name = name;
+        this.salary = salary;
+        this.joiningDate = joiningDate;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Date getJoiningDate() {
+        return joiningDate;
+    }
+
+    public void setJoiningDate(Date joiningDate) {
+        this.joiningDate = joiningDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                ", joiningDate=" + joiningDate +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
---
+++ src/StreamExample.java
@@ -0,0 +1,59 @@
+package src;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StreamExample {
+
+    public static void main(String[] args) throws ParseException {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Date joiningDateRam = dateFormat.parse("01-01-2000");
+        Date joiningDateSita = dateFormat.parse("15-05-1995");
+        Date joiningDateVishnu = dateFormat.parse("10-12-1998");
+        Date joiningDateMahesh = dateFormat.parse("20-03-1990");
+
+        List<Employee> emp = Arrays.asList(
+                new Employee("Ram", 50000.0, joiningDateRam, "M"),
+                new Employee("Sita", 40000.0, joiningDateSita, "F"),
+                new Employee("Vishnu", 20000.0, joiningDateVishnu, "M"),
+                new Employee("Mahesh", 70000.0, joiningDateMahesh, "M")
+        );
+
+        /** *
+         *
+         * 1. Find the maximum salary of the employee.
+         * **/
+        Optional<Employee> maxSalary = emp.stream().max(Comparator.comparingDouble(Employee::getSalary));
+        maxSalary.ifPresent(System.out::println);
+
+        /**
+         *
+         * 2. Find the second-highest salary
+         */
+        Optional<Employee> secondHighestSalary = emp.stream()
+                .sorted((a, b)-> Double.compare(b.getSalary(), a.getSalary()))
+                .skip(1)
+                .findFirst();
+        secondHighestSalary.ifPresent(System.out::println);
+
+        /**
+         *
+         * 3. Find senior Employee based on joining date
+         */
+        Optional<Employee> seniorEmp = emp.stream()
+                .min(Comparator.comparing(Employee::getJoiningDate));
+        seniorEmp.ifPresent(System.out::println);
+
+        /**
+         *
+         * 4. Count the employee based on the gender
+         */
+        Map<String, Long> genderCount = emp.stream()
+                        .collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
+        System.out.println(genderCount);
+
+    }
+}
